012396baa99f59e5e12f3a0da9ab49df0b943685
Rik van der Kooij <rikvanderkooij@gmail.com>
2026-01-14 22:15:44 +0100

    Add persistence to disk

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::error::Error;
 use std::fmt::{self, Display};
-use std::io;
+use std::fs::OpenOptions;
+use std::io::{self, Read, Seek, SeekFrom, Write};
+use std::os::unix::fs::OpenOptionsExt;
+use std::path::Path;
 use std::str::FromStr;
 
 enum Statement {
@@ -97,27 +100,115 @@ impl Display for Row {
     }
 }
 
-#[derive(Default)]
+struct Pager {
+    file: std::fs::File,
+    pages: Vec<Option<Box<[u8; Pager::SIZE]>>>,
+}
+
+impl Pager {
+    const SIZE: usize = 4096;
+
+    fn new(path: impl AsRef<Path>) -> Result<Self, Box<dyn Error>> {
+        let file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(false)
+            .mode(0o0600)
+            .open(path)?;
+
+        let file_length = file.metadata()?.len();
+        let page_count = usize::try_from(file_length.div_ceil(Self::SIZE as u64))?;
+
+        Ok(Self {
+            file,
+            pages: vec![None; page_count],
+        })
+    }
+
+    fn get_page(&mut self, page_num: usize) -> Result<&mut [u8; Self::SIZE], Box<dyn Error>> {
+        if page_num >= self.pages.len() {
+            self.pages.resize(page_num + 1, None);
+        }
+
+        if self.pages[page_num].is_none() {
+            let mut page = Box::new([0u8; Self::SIZE]);
+
+            let file_length = self.file.metadata()?.len();
+            let num_pages = file_length.div_ceil(Self::SIZE as u64);
+
+            if (page_num as u64) < num_pages {
+                let offset = page_num as u64 * Self::SIZE as u64;
+                let bytes_to_read = usize::try_from(std::cmp::min(
+                    Self::SIZE as u64,
+                    file_length.saturating_sub(offset),
+                ))?;
+
+                self.file.read_exact(&mut page[..bytes_to_read])?;
+            }
+
+            self.pages[page_num] = Some(page);
+        }
+
+        let page = self.pages[page_num]
+            .as_deref_mut()
+            .expect("page must be initialized before returning");
+
+        Ok(page)
+    }
+
+    fn flush_page(&mut self, index: usize, size: usize) -> io::Result<()> {
+        let Some(page) = &self.pages[index] else {
+            return Ok(());
+        };
+
+        let offset = (index as u64) * (Self::SIZE as u64);
+
+        self.file.seek(SeekFrom::Start(offset))?;
+        self.file.write_all(&page[..size])
+    }
+}
+
 struct Table {
     row_count: usize,
-    pages: Vec<[u8; Self::PAGE_SIZE]>,
+    pager: Pager,
 }
 
 impl Table {
-    const PAGE_SIZE: usize = 4096;
-    const ROWS_PER_PAGE: usize = Self::PAGE_SIZE / Row::SIZE;
+    const ROWS_PER_PAGE: usize = Pager::SIZE / Row::SIZE;
 
-    fn insert(&mut self, row: &Row) {
-        let page_num = self.row_count / Self::ROWS_PER_PAGE;
+    fn new(path: impl AsRef<Path>) -> Result<Self, Box<dyn Error>> {
+        let pager = Pager::new(path)?;
+        let file_length = usize::try_from(pager.file.metadata()?.len())?;
+        let row_count = file_length / Row::SIZE;
 
-        if self.pages.len() <= page_num {
-            self.pages.push([0u8; Self::PAGE_SIZE]);
+        Ok(Self { row_count, pager })
+    }
+
+    fn close(&mut self) -> io::Result<()> {
+        let full_page_count = self.row_count / Self::ROWS_PER_PAGE;
+        for i in 0..full_page_count {
+            if self.pager.pages[i].is_some() {
+                self.pager.flush_page(i, Pager::SIZE)?;
+            }
         }
+
+        let additional_row_count = self.row_count % Self::ROWS_PER_PAGE;
+        if additional_row_count > 0 {
+            self.pager
+                .flush_page(full_page_count, additional_row_count * Row::SIZE)?;
+        }
+
+        self.pager.file.sync_all()
+    }
+
+    fn insert(&mut self, row: &Row) -> Result<(), Box<dyn Error>> {
+        let page_num = self.row_count / Self::ROWS_PER_PAGE;
 
         let row_offset = self.row_count % Self::ROWS_PER_PAGE;
         let byte_offset = row_offset * Row::SIZE;
 
-        let page = &mut self.pages[page_num];
+        let page = self.pager.get_page(page_num)?;
 
         page[byte_offset..byte_offset + Row::ID_SIZE].copy_from_slice(&row.id.to_le_bytes());
 
@@ -128,31 +219,29 @@ impl Table {
         page[email_offset..email_offset + Row::EMAIL_SIZE].copy_from_slice(&row.email);
 
         self.row_count += 1;
+
+        Ok(())
     }
 
-    fn select<W>(&self, output: &mut W) -> Result<(), Box<dyn Error>>
+    fn select<W>(&mut self, output: &mut W) -> Result<(), Box<dyn Error>>
     where
         W: io::Write,
     {
         for i in 0..self.row_count {
-            let row = self.deserialize_row(i);
+            let row = self.deserialize_row(i)?;
             writeln!(output, "{row}")?;
         }
 
         Ok(())
     }
 
-    fn deserialize_row(&self, index: usize) -> Row {
+    fn deserialize_row(&mut self, index: usize) -> Result<Row, Box<dyn Error>> {
         let page_num = index / Self::ROWS_PER_PAGE;
         let row_offset = index % Self::ROWS_PER_PAGE;
         let byte_offset = row_offset * Row::SIZE;
 
-        let page = &self.pages[page_num];
-        let id = u32::from_le_bytes(
-            page[byte_offset..byte_offset + Row::ID_SIZE]
-                .try_into()
-                .unwrap(),
-        );
+        let page = self.pager.get_page(page_num)?;
+        let id = u32::from_le_bytes(page[byte_offset..byte_offset + Row::ID_SIZE].try_into()?);
 
         let mut username = [0; Row::USERNAME_SIZE];
         let mut email = [0; Row::EMAIL_SIZE];
@@ -163,11 +252,11 @@ impl Table {
         let email_offset = username_offset + Row::USERNAME_SIZE;
         email.copy_from_slice(&page[email_offset..email_offset + Row::EMAIL_SIZE]);
 
-        Row {
+        Ok(Row {
             id,
             username,
             email,
-        }
+        })
     }
 }
 
@@ -192,7 +281,7 @@ where
 {
     match statement {
         Statement::Insert(row) => {
-            table.insert(row);
+            table.insert(row)?;
             Ok(())
         }
         Statement::Select => table.select(output),
@@ -223,12 +312,12 @@ fn do_meta_command(command: &str) -> Result<RunControl, MetaCommandResult> {
     }
 }
 
-fn run<R, W>(input: &mut R, output: &mut W) -> Result<(), Box<dyn Error>>
+fn run<R, W>(input: &mut R, output: &mut W, path: impl AsRef<Path>) -> Result<(), Box<dyn Error>>
 where
     R: io::BufRead,
     W: io::Write,
 {
-    let mut table = Table::default();
+    let mut table = Table::new(path)?;
     let mut input_buffer = String::new();
 
     loop {
@@ -242,7 +331,10 @@ where
 
         if command.starts_with('.') {
             match do_meta_command(command) {
-                Ok(RunControl::Exit) => return Ok(()),
+                Ok(RunControl::Exit) => {
+                    table.close()?;
+                    return Ok(());
+                }
                 Err(MetaCommandResult::UnrecognizedCommand) => {
                     writeln!(output, "Unrecognized command '{command}'")?;
                 }
@@ -273,18 +365,22 @@ where
 fn main() -> Result<(), Box<dyn Error>> {
     let mut stdin = io::stdin().lock();
     let mut stdout = io::stdout().lock();
-    run(&mut stdin, &mut stdout)
+    run(&mut stdin, &mut stdout, "mysqlite.db")
 }
 
 #[cfg(test)]
 mod tests {
+    use std::path::{Path, PathBuf};
+
+    use tempfile::TempDir;
+
     use super::{Error, io, run};
 
     #[test]
     fn test_simple_insert_and_select() {
         let scripts = ["insert 1 user1 person1@example.com", "select", ".exit"];
-        let output = run_scripts(&scripts).unwrap();
-        let output = std::str::from_utf8(&output).unwrap();
+        let (_dir, path) = create_test_db_file();
+        let output = run_scripts(&scripts, &path).unwrap();
 
         assert_eq!(
             output,
@@ -299,8 +395,8 @@ mod tests {
             "select",
             ".exit",
         ];
-        let output = run_scripts(&scripts).unwrap();
-        let output = std::str::from_utf8(&output).unwrap();
+        let (_dir, path) = create_test_db_file();
+        let output = run_scripts(&scripts, &path).unwrap();
 
         assert_eq!(
             output,
@@ -313,10 +409,10 @@ mod tests {
             "insert 1 abcdefghijklmnopqrstuvwxyzabcdefg a@b.com",
             ".exit",
         ];
-        let output = run_scripts(&scripts).unwrap();
-        let output = std::str::from_utf8(&output).unwrap();
+        let (_dir, path) = create_test_db_file();
+        let output = run_scripts(&scripts, &path).unwrap();
 
-        assert_eq!(output, "mysqlite> String is too long.\nmysqlite> ",);
+        assert_eq!(output, "mysqlite> String is too long.\nmysqlite> ");
     }
 
     #[test]
@@ -324,10 +420,10 @@ mod tests {
         let n = 255;
         let insert_str = &format!("insert 1 u {0:a<1$}", "", n);
         let scripts = [insert_str, ".exit"];
-        let output = run_scripts(&scripts).unwrap();
-        let output = std::str::from_utf8(&output).unwrap();
+        let (_dir, path) = create_test_db_file();
+        let output = run_scripts(&scripts, &path).unwrap();
 
-        assert_eq!(output, "mysqlite> mysqlite> ",);
+        assert_eq!(output, "mysqlite> mysqlite> ");
     }
 
     #[test]
@@ -335,19 +431,40 @@ mod tests {
         let n = 256;
         let insert_str = &format!("insert 1 u {0:a<1$}", "", n);
         let scripts = [insert_str, ".exit"];
-        let output = run_scripts(&scripts).unwrap();
-        let output = std::str::from_utf8(&output).unwrap();
+        let (_dir, path) = create_test_db_file();
+        let output = run_scripts(&scripts, &path).unwrap();
 
-        assert_eq!(output, "mysqlite> String is too long.\nmysqlite> ",);
+        assert_eq!(output, "mysqlite> String is too long.\nmysqlite> ");
     }
 
-    fn run_scripts(commands: &[&str]) -> Result<Vec<u8>, Box<dyn Error>> {
+    #[test]
+    fn test_persistent_data() {
+        let scripts = ["insert 1 user1 person1@example.com", ".exit"];
+        let (_dir, path) = create_test_db_file();
+        let output = run_scripts(&scripts, &path).unwrap();
+        assert_eq!(output, "mysqlite> mysqlite> ");
+
+        let scripts = ["select", ".exit"];
+        let output = run_scripts(&scripts, &path).unwrap();
+        assert_eq!(
+            output,
+            "mysqlite> (1 user1 person1@example.com)\nmysqlite> "
+        );
+    }
+
+    fn run_scripts(commands: &[&str], path: &impl AsRef<Path>) -> Result<String, Box<dyn Error>> {
         let input = String::from(commands.join("\n"));
         let mut input = io::Cursor::new(&input[..]);
         let mut output = vec![];
 
-        run(&mut input, &mut output)?;
+        run(&mut input, &mut output, path)?;
 
-        Ok(output)
+        Ok(std::str::from_utf8(&output)?.into())
+    }
+
+    fn create_test_db_file() -> (TempDir, PathBuf) {
+        let dir = TempDir::new().unwrap();
+        let path = dir.path().join("test.db");
+        return (dir, path);
     }
 }
